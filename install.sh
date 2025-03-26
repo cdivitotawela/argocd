b9611afe2b0dc0bbb74df18fe474f60cbe30adf4
Chaminda Divitotawela <cdivitotawela@gmail.com>
2025-03-26 12:34:28 +1000

    Installation update

--- install.sh
+++ install.sh
@@ -19,9 +19,29 @@ kubectl apply -n $ARGOCD_NAMESPACE -f https://raw.githubusercontent.com/argoproj
 kubectl -n $ARGOCD_NAMESPACE wait pod -l app.kubernetes.io/name=argocd-server --for=condition=Ready
 
 # Extract the admin passowrd
-echo -n "Admin password: "
-kubectl -n $ARGOCD_NAMESPACE get secrets -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d
+kubectl -n $ARGOCD_NAMESPACE get secrets -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d > .password
+echo "====================="
+echo "Admin Password"
+cat .password
+echo
+echo "====================="
 
 # Port forward
-kubectl -n $ARGOCD_NAMESPACE port-forward svc/argocd-server 8443:443 &
-open -a "Google Chrome" "https://localhost:8443/"
+kubectl -n $ARGOCD_NAMESPACE port-forward svc/argocd-server 8443:443 > /dev/null 2>&1 &
+
+# Login to argocd
+_PASSWORD=$(cat .password)
+argocd login 127.0.0.1:8443 --username admin --password $_PASSWORD --insecure
+
+# Add repository
+git remote get-url origin | grep -q "git@" && {
+  GIT_URL=$(git remote get-url origin | sed 's|git@\([^:]*\):\(.*\)|https://\1/\2|g')
+} || {
+  GIT_URL=(git remote get-url origin)
+}
+
+sleep 10
+echo "GIT URL: $GIT_URL"
+argocd repo add $GIT_URL --name argocd
+
+#open -a "Google Chrome" "https://localhost:8443/"
